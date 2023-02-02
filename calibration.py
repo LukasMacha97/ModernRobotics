70909103a6fa9b52a88189384801520b623233c9
Lukas Macha <lm2292@bath.ac.uk>
2023-02-02 15:23:03 +0000

    adding calibration for ndof robot

--- calibration.py
+++ calibration.py
@@ -102,7 +102,7 @@ def a_matrix( kesi, q ):
 
     w=kesi[0:3]
     v=kesi[3:6]
-    bW=np.zeros((6,6))
+    bW=np.zeros((6,6))  # change when using ndof
     bW[0:3,0:3]=mr.VecToso3(w)
     bW[3:6,0:3]=mr.VecToso3(v)
     bW[3:6,3:6]=mr.VecToso3(w)
@@ -111,7 +111,7 @@ def a_matrix( kesi, q ):
     if n==0:
         aM=q*np.eye(6)
     else:
-        a0 = q * np.eye(6)
+        a0 = q * np.eye(6)  # change when using different ndof
 
         a10 = ( ( 4 - t * np.sin(t) - 4 * np.cos(t) ) / 2 / n**2 )
         a11 = np.dot(a10,bW) 
@@ -211,13 +211,6 @@ def se_3_exp( kesi ):
     return T
 
 
-def f_kin(xi,theta,n):
-    
-    T = np.eye(4)
-    for i in range(0,n):
-        T = np.dot(T,se_3_exp(np.dot(xi[:,i],theta[i])))
-    
-    return T
 
 def traditional_calibration_6dof(xi0, xist0, vtheta, gm, M):
    
@@ -316,7 +309,98 @@ def traditional_calibration_6dof(xi0, xist0, vtheta, gm, M):
     return xi, xist
 
 
-def traditionalCalibrationScara(xi0, vtheta, gm, M):
+def traditional_calibration_5dof(xi0, xist0, vtheta, gm, M):
+   
+    xi = np.copy(xi0)
+
+    xist = np.copy(xist0)
+
+    N = np.size(vtheta,0)
+    
+    n = np.size(vtheta,1)
+
+    gn = np.zeros((4,4,N))
+    dg = np.zeros((4,4,N))
+    v_Log = np.zeros((6,N))
+
+    for m in range(0,M):
+
+        for i in range(0,N):
+
+            gn[:,:,i]= mr.FKinSpace(se_3_exp(xist),xi,vtheta[i,:])
+            dg[:,:,i] = np.linalg.solve(gn[:,:,i].T, gm[:,:,i].T).T
+            v_Log[:,i]= v_log(dg[:,:,i])
+
+        simY = np.zeros((600, 1))
+
+        for i in range(1, N+1):
+
+            simY[6*i - 6: 6*i, 0] = v_Log[:,i-1]
+
+
+        simJ = np.zeros((600,36))
+
+        for k in range(0,N):
+
+            mat1 = se_3_exp( np.dot(vtheta[k,0], xi[:,0]))
+            a_mat1 = a_matrix(xi[:,0], vtheta[k,0])
+
+            mat2 = se_3_exp( np.dot(vtheta[k,1], xi[:,1]))
+            a_mat2 = a_matrix(xi[:,1], vtheta[k,1])
+
+            mat3 = se_3_exp( np.dot(vtheta[k,2], xi[:,2]))
+            a_mat3 = a_matrix(xi[:,2], vtheta[k,2])
+
+            mat4 = se_3_exp( np.dot(vtheta[k,3], xi[:,3]))
+            a_mat4 = a_matrix(xi[:,3], vtheta[k,3])
+
+            mat5 = se_3_exp( np.dot(vtheta[k,4], xi[:,4]))
+            a_mat5 = a_matrix(xi[:,4], vtheta[k,4])
+
+            a_mat_st = a_matrix_st(xist)
+
+            simJ[6*k : 6+6*k, 0:6  ] = a_mat1
+            simJ[6*k : 6+6*k, 6:12 ] = np.dot(mr.Adjoint(mat1), a_mat2)
+            simJ[6*k : 6+6*k, 12:18] = np.dot(mr.Adjoint(np.linalg.multi_dot([mat1, mat2])), a_mat3)
+            simJ[6*k : 6+6*k, 18:24] = np.dot(mr.Adjoint(np.linalg.multi_dot([mat1, mat2, mat3])), a_mat4)
+            simJ[6*k : 6+6*k, 24:30] = np.dot(mr.Adjoint(np.linalg.multi_dot([mat1, mat2, mat3, mat4])), a_mat5)
+            simJ[6*k : 6+6*k, 30:36] = np.dot(mr.Adjoint(np.linalg.multi_dot([mat1, mat2, mat3, mat4, mat5])), a_mat_st)
+
+        dp = np.dot(np.linalg.pinv(simJ),simY)
+
+        dp = dp.reshape((len(dp),))
+
+        xi[:,0] = (xi[:,0].reshape((6,1)) + dp[0:6].reshape((6,1))).reshape((6,))
+        xi[0:3,0] = xi[0:3,0] / np.linalg.norm(xi[0:3,0])
+        xi[3:6,0] = xi[3:6,0] + np.dot( np.dot(xi[0:3,1].T, xi[3:6,0]) /  np.dot(xi[0:3,0].T, xi[0:3,0] ), xi[0:3,0])
+
+        xi[:,1] = (xi[:,1].reshape((6,1)) + dp[6:12].reshape((6,1))).reshape((6,))
+        xi[0:3,1] = xi[0:3,1]/np.linalg.norm(xi[0:3,1])
+        xi[3:6,1] = xi[3:6,1] - np.dot( np.dot(xi[0:3,1].T, xi[3:6,1]) / np.dot(xi[0:3,1].T,xi[0:3,1]), xi[0:3,1])
+
+        xi[:,2] = (xi[:,2].reshape((6,1)) + dp[12:18].reshape((6,1))).reshape((6,))
+        xi[0:3,2] = xi[0:3,2]/np.linalg.norm(xi[0:3,2])
+        xi[3:6,2] = xi[3:6,2] - np.dot( np.dot(xi[0:3,2].T, xi[3:6,2]) / np.dot(xi[0:3,2].T,xi[0:3,2]), xi[0:3,2])
+
+        xi[:,3]= (xi[:,3].reshape((6,1)) + dp[18:24].reshape((6,1))).reshape((6,))
+        xi[0:3,3]=xi[0:3,3] / np.linalg.norm(xi[0:3,3])
+        xi[3:6,3]= xi[3:6,3] - np.dot(np.dot(xi[0:3,3].T, xi[3:6,3]) / np.dot(xi[0:3,3].T,xi[0:3,3]),xi[0:3,3])
+
+        xi[:,4]= (xi[:,4].reshape((6,1)) + dp[24:30].reshape((6,1))).reshape((6,))
+        xi[0:3,4]=xi[0:3,4] / np.linalg.norm(xi[0:3,4])
+        xi[3:6,4]= xi[3:6,4] - np.dot(np.dot(xi[0:3,4].T, xi[3:6,4]) / np.dot(xi[0:3,4].T,xi[0:3,4]),xi[0:3,4])
+
+        # xi[:,5]= (xi[:,5].reshape((6,1)) + dp[30:36].reshape((6,1))).reshape((6,))
+        # xi[0:3,5]=xi[0:3,5] / np.linalg.norm(xi[0:3,5])
+        # xi[3:6,5]= xi[3:6,5] - np.dot(np.dot(xi[0:3,5].T, xi[3:6,5]) / np.dot(xi[0:3,5].T,xi[0:3,5]),xi[0:3,5])
+
+        xist = xist + dp[30:36]
+
+    return xi, xist
+
+
+
+def traditional_calibration_scara(xi0, vtheta, gm, M):
 
     xi = np.copy(xi0)
 
